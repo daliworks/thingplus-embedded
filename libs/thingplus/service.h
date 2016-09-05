fea5fd5b250a127cc12c707f0ab17359382ac0dc
Younghyun Jo <younghyun.jo@daliworks.net>
2016-09-05 17:32:13 +0900

    delete previous library code

--- libs/thingplus/service.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SERVICE_H_
-#define _SERVICE_H_
-
-#include "thingplus.h"
-
-char *service_do(void *s, char *payload);
-void *service_init(struct thingplus_callback *cb, void *cb_arg);
-void service_cleanup(void *s);
-#endif //#define _SERVICE_H_
